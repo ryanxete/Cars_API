e4e18b5771feb6474baa1f04da37cb03e95bfb89
Ryan Etedali <ryanetedali@gmail.com>
2022-07-13 18:20:03 -0500

    completed post functionality

--- cars/views.py
+++ cars/views.py
@@ -3,10 +3,16 @@ from rest_framework.response import Response
 from .serializers import CarSerializer
 from .models import Car
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def cars_list(request):
-    cars = Car.objects.all()
-
-    serializer = CarSerializer(cars, many=True)
-
-    return Response(serializer.data)
+    if request.method == "GET":
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = CarSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
